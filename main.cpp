92c40020f1a47d77ad8b7e46657ccc5247c4514f
Jaedson Silva <contacanal798@gmail.com>
2022-06-30 22:12:23 -0300

    adding writeFile function in main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,30 @@ using namespace std;
 void writeFile();
 void readFile();
 
+void writeFile() {
+    // output file stream
+    ofstream file("file.txt", ios::app);
+
+    if(!file.is_open()) {
+        cout << "Error to open file!" << endl;
+    } else {
+        string inputValue;
+
+        while(true) {
+            cout << "Writing in file: ";
+            getline(cin, inputValue);
+
+            if(inputValue == "999") {
+                cout << "------------" << endl;
+                cout << "Written file" << endl;
+                break;
+            } else {
+                file << inputValue << endl;
+            }
+        }
+    }
+}
+
 void readFile() {
     // input file stream
     ifstream file("file.txt", ios::out);
